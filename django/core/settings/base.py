0622f9aae177888f92fe95871a85c165649cbf64
Pedro A. Neves <PedroANeves@users.noreply.github.com>
2022-07-19 20:58:34 -0300

    move development configs

--- django/core/settings/base.py
+++ django/core/settings/base.py
@@ -20,14 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,22 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
-
-CHANNEL_LAYERS={
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-     }
-}
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
--- django/core/settings/develop.py
+++ django/core/settings/develop.py
@@ -3,3 +3,22 @@ import os
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'TKaVmY6Uzay9VgR9lH7aOKdpzVsTlGizClcFphcieHI'
+
+CHANNEL_LAYERS={
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+     }
+}
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
